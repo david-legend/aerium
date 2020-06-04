c12ead0c83a3390266c8967d90247e8c0526a8eb
David <davidcobbina47@gmail.com>
2020-06-04 11:42:17 +0000

    added message_page_mobile

--- lib/presentation/pages/message/message_page_mobile.dart
+++ lib/presentation/pages/message/message_page_mobile.dart
@@ -4,7 +4,12 @@ import 'package:portfoliosite/presentation/widgets/custom_text_form_field.dart';
 import 'package:portfoliosite/presentation/widgets/spaces.dart';
 import 'package:portfoliosite/values/values.dart';
 
-class MessagePageMobile extends StatelessWidget {
+class MessagePageMobile extends StatefulWidget {
+  @override
+  _MessagePageMobileState createState() => _MessagePageMobileState();
+}
+
+class _MessagePageMobileState extends State<MessagePageMobile> {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
@@ -36,6 +41,7 @@ class MessagePageMobile extends StatelessWidget {
               ],
             ),
           ),
+          SpaceH16(),
           _buildForm(),
         ],
       ),
@@ -43,7 +49,8 @@ class MessagePageMobile extends StatelessWidget {
   }
 
   Widget _buildForm() {
-    Container(
+    ThemeData theme = Theme.of(context);
+    return Container(
       padding: const EdgeInsets.symmetric(
         horizontal: Sizes.PADDING_16,
 //        vertical: Sizes.PADDING_16,
@@ -53,22 +60,47 @@ class MessagePageMobile extends StatelessWidget {
           CustomTextFormField(
             filled: true,
             fillColor: AppColors.grey100,
+            hintText: StringConst.NAME_HINT_TEXT,
           ),
-          SpaceH8(),
+          SpaceH16(),
           CustomTextFormField(
             filled: true,
             fillColor: AppColors.grey100,
+            hintText: StringConst.PHONE_HINT_TEXT,
           ),
-          SpaceH8(),
+          SpaceH16(),
           CustomTextFormField(
             filled: true,
             fillColor: AppColors.grey100,
+            hintText: StringConst.EMAIL_HINT_TEXT,
           ),
-          SpaceH8(),
+          SpaceH16(),
           CustomTextFormField(
             filled: true,
             fillColor: AppColors.grey100,
+            hintText: StringConst.MESSAGE_HINT_TEXT,
+            maxLines: 12,
           ),
+          SpaceH40(),
+          FlatButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    StringConst.SEND_MESSAGE,
+                    style: theme.textTheme.headline6
+                        .copyWith(color: AppColors.grey100),
+                  ),
+                  SpaceW12(),
+                  CircularContainer(
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: AppColors.deepBlue,
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -17,6 +17,8 @@ class StringConst {
 
   static const String PROJECT_NAME = "Project Name";
 
+
+
   static const String DEV_NAME = "DAVID COBBINA";
   static const String SPECIALITY = "FLUTTER DEV";
 
@@ -26,7 +28,14 @@ class StringConst {
 
   static const String ABOUT_DEV_TEXT = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.";
 
+  //Form Hint Texts
+  static const String NAME_HINT_TEXT = "Name";
+  static const String PHONE_HINT_TEXT = "Phone (Optional)";
+  static const String EMAIL_HINT_TEXT = "Email";
+  static const String MESSAGE_HINT_TEXT = "Message";
 
+
+  static const String SEND_MESSAGE = "Send Message";
 
 
 }
